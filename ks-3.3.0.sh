8a3fe6809f54933e0b612e49936eb5c53247d0b5
wuxs <wuxs@wuxs.vip>
2022-09-15 19:41:14 +0800

    Create ks-3.3.0.sh

---
+++ ks-3.3.0.sh
@@ -0,0 +1,13 @@
+#/bin/bash
+
+curl https://get.docker.com |bash
+
+apt install socat conntrack ebtables ipset
+
+export KKZONE=cn
+
+curl -sfL https://get-kk.kubesphere.io | VERSION=v2.2.1 sh -
+
+chmod +x kk
+
+./kk create cluster --with-kubernetes v1.22.10 --with-kubesphere v3.3.0
