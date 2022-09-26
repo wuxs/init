1b0d1d544de422f02b334bb0d08aa86b70ba8076
wuxs <wuxs@wuxs.vip>
2022-09-26 16:50:44 +0800

    Update staging.sh

--- staging.sh
+++ staging.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
  
 if [ $ALL_PROXY == "" ];then
-  echo "export ALL_RPOXY=http://127.0.0.1:7890" > /etc/profile
+  echo "export ALL_PROXY=http://127.0.0.1:7890" > /etc/profile
   source /et/profile
 fi
