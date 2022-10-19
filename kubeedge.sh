8a8b593cb91e6f6fe14bbaeffa217d424a867db4
wuxs <wuxs@wuxs.vip>
2022-10-19 15:39:58 +0800

    Update kubeedge.sh

--- kubeedge.sh
+++ kubeedge.sh
@@ -1,8 +1,8 @@
 #! /bin/bash
-echo "net.ipv4.ip_forward = 1" >> /etc/sysctl.conf
+sed -i '/^#\{0,1\}net.ipv4.ip_forward/c\net.ipv4.ip_forward = 1' /etc/sysctl.conf
 sudo sysctl -p | grep ip_forward
 
-echo "hosts:          dns files mdns4_minimal [NOTFOUND=return]" >> /etc/nsswitch.conf
+sed -i '/^hosts/c\hosts:          dns files mdns4_minimal [NOTFOUND=return]' /etc/nsswitch.conf
 cat /etc/nsswitch.conf
 
-curl https://get.docker.com |bash
+curl https://get.docker.com | bash
