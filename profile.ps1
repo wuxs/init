6884c00ed44188e3f4935c3f6b607ffe38c98c02
wuxs <wuxs@wuxs.vip>
2022-10-24 10:27:34 +0800

    Add files via upload

---
+++ profile.ps1
@@ -0,0 +1,4 @@
+# Reverse Search
+Set-PSReadLineOption -HistorySearchCursorMovesToEnd
+Set-PSReadlineKeyHandler -Key UpArrow -Function HistorySearchBackward
+Set-PSReadlineKeyHandler -Key DownArrow -Function HistorySearchForward
