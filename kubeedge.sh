c7e7a90e092a1245ced67cf4ff46d81e612a5536
wuxs <wuxs@wuxs.vip>
2022-12-19 17:59:29 +0800

    Update kubeedge.sh

--- kubeedge.sh
+++ kubeedge.sh
@@ -6,3 +6,5 @@ sed -i '/^hosts/c\hosts:          dns files mdns4_minimal [NOTFOUND=return]' /et
 cat /etc/nsswitch.conf
 
 curl https://get.docker.com | bash
+
+wget https://github.com/kubeedge/kubeedge/releases/download/v1.12.1/keadm-v1.12.1-linux-amd64.tar.gz && tar zxvf keadm-v1.12.1-linux-amd64.tar.gz && chmod +x keadm-v1.12.1-linux-amd64/keadm/keadm && mv keadm-v1.12.1-linux-amd64/keadm/keadm /usr/local/bin && rm -r keadm-v1.12.1-linux-amd64.tar.gz keadm-v1.12.1-linux-amd64
