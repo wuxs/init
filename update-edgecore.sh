7534306a6bf94c98371d34d2f865215a68d97bce
wuxs <wuxs@wuxs.vip>
2022-12-22 11:18:51 +0800

    Create update-edgecore.sh

---
+++ update-edgecore.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+echo "bash update-edgecore.sh <0-9> $TOKEN"
+
+
+[ ! -f /etc/kubeedge/config/edgecore.yaml.bak ] && cp /etc/kubeedge/config/edgecore.yaml /etc/kubeedge/config/edgecore.yaml.bak
+
+service edgecore stop
+
+sed -re "s/300[0-9]([0-4])/300$1\1/g" /etc/kubeedge/config/edgecore.yaml > /tmp/edgecore.yaml
+cp /tmp/edgecore.yaml  /etc/kubeedge/config/edgecore.yaml
+sed -re "s/token.+/token: $2/g"  /etc/kubeedge/config/edgecore.yaml > /tmp/edgecore.yaml
+cp /tmp/edgecore.yaml  /etc/kubeedge/config/edgecore.yaml
+
+rm /etc/kubeedge/ca/*
+rm /etc/kubeedge/certs/*
+rm /var/lib/kubeedge/edgecore.db
+
+service edgecore start
