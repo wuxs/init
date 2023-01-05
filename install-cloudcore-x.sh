8f6c49a0dea2b87960096b6830bb4509c4d28133
wuxs <wuxs@wuxs.vip>
2023-01-06 00:10:00 +0800

    Update install-cloudcore-x.sh

--- install-cloudcore-x.sh
+++ install-cloudcore-x.sh
@@ -1,13 +1,14 @@
 #!/usr/bin/env bash
+# https://init.wuxs.vip/install-cloudcore-x.sh
 
-if [ $# !=2 ];then
+if [ $# != 2 ];then
   echo 'use "bash install-cloudcore-x.sh x" to install cloudcore with ports 300x0-300x4. (x is a number between 0 and 9)'
   exit 1
 fi
 
 [ ! -d kubeedge ] && git clone https://github.com/kubeedge/kubeedge
 
-[ ! -f kubeedge.values.yaml] && wget https://init.wuxs.vip/kubeedge.values.yaml -O kubeedge.values.yaml
+[ ! -f kubeedge.values.yaml ] && wget https://init.wuxs.vip/kubeedge.values.yaml -O kubeedge.values.yaml
 
 sed -re "s/300[0-9]([0-4])/300$1\1/g" kubeedge.values.yaml > temp.yaml
 
