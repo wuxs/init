602d6a1c63f37fcc62abd088949b28fbbbf925df
wuxs <wuxs@wuxs.vip>
2023-09-25 14:23:00 +0800

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 echo "install inputrc..."
-curl https://init.wuxs.vip/inputrc --output ~/.inputrc -q
+curl https://init.wuxs.vip/inputrc --output ~/.inputrc -s
 
 echo "Please log in again to make inputrc take effect."
