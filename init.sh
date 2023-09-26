6c88d889ae99ffdef8df62c3b049d03de668b7f4
wuxs <wuxs@wuxs.vip>
2023-09-26 10:37:38 +0800

    Update init.sh

--- init.sh
+++ init.sh
@@ -4,7 +4,6 @@ echo "install inputrc..."
 curl https://init.wuxs.vip/inputrc --output ~/.inputrc -s
 
 echo "Please log in again to make inputrc take effect."
-echo ""
 
 read -p "是否安装Kubernetes？(y/n，默认为n): " answer
 
