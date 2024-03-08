e254877bd576f3680bc1996bde217a3b7fc76573
wuxs <wuxs@wuxs.vip>
2024-03-08 11:18:30 +0000

    Update fix-image-pull.sh

--- fix-image-pull.sh
+++ fix-image-pull.sh
@@ -9,6 +9,23 @@ else
   runtime="crictl"
 fi
 
+complete_image_name() {
+    local image_name="$1"
+    
+    # 检查镜像名称是否包含用户名
+    if [[ $image_name != *"/"* ]]; then
+        # 如果镜像名称中不包含斜杠，但包含冒号，添加默认的用户名 "library"
+        if [[ $image_name == *":"* ]]; then
+            image_name="library/$image_name"
+        else
+            # 如果镜像名称中不包含斜杠和冒号，添加默认的用户名 "library" 和标签 "latest"
+            image_name="library/$image_name:latest"
+        fi
+    fi
+    
+    echo "$image_name"
+}
+
 # 使用 kubectl 命令获取所有 Pod 的信息，并筛选出状态为 ImagePullBackOff 的 Pod
 kubectl get pods --all-namespaces -o=jsonpath='{range .items[*]}{.metadata.namespace}{"\t"}{.metadata.name}{"\t"}{.status.containerStatuses[*].state.waiting.reason}{"\n"}{end}' | grep ImagePullBackOff | while IFS=$'\t' read -r namespace pod reason; do
     # 获取镜像名称列表
@@ -17,6 +34,7 @@ kubectl get pods --all-namespaces -o=jsonpath='{range .items[*]}{.metadata.names
     # 遍历处理每个镜像
     while IFS= read -r image; do
         echo "Image <$image> ImagePullBackOff, pull image with proxy docker.wuxs.workers.dev"
+        image=$(complete_image_name $image)
         image_name=$(echo $image | cut -d':' -f1)
         image_tag=$(echo $image | cut -d':' -f2)
         echo "$runtime pull docker.wuxs.workers.dev/$image_name:$image_tag"
