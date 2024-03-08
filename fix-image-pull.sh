8a14b0f2036ff7bc47edc19b99640c02aba6e316
wuxs <wuxs@wuxs.vip>
2024-03-08 11:08:38 +0000

    Update fix-image-pull.sh

--- fix-image-pull.sh
+++ fix-image-pull.sh
@@ -10,14 +10,18 @@ else
 fi
 
 # 使用 kubectl 命令获取所有 Pod 的信息，并筛选出状态为 ImagePullBackOff 的 Pod
-kubectl get pods --all-namespaces -o=jsonpath='{range .items[*]}{.metadata.namespace}{"\t"}{.metadata.name}{"\t"}{.status.containerStatuses[*].state.waiting.reason}{"\n"}{end}' | grep ImagePullBackOff | while read namespace pod reason; do
-    # 获取镜像名称
-    image=$(kubectl get pod $pod -n $namespace -o=jsonpath='{.spec.containers[*].image}')
-    echo "Image <$image> ImagePullBackOff, pull image with proxy docker.wuxs.workers.dev"
-    image_name=$(echo $image | cut -d':' -f1)
-    image_tag=$(echo $image | cut -d':' -f2)
-    echo "$runtime pull docker.wuxs.workers.dev/$image_name:$image_tag"
-    bash -c "$runtime pull docker.wuxs.workers.dev/$image_name:$image_tag"
-    echo "$runtime tag docker.wuxs.workers.dev/$image_name:$image_tag $image"
-    bash -c "$runtime tag docker.wuxs.workers.dev/$image_name:$image_tag $image"
+kubectl get pods --all-namespaces -o=jsonpath='{range .items[*]}{.metadata.namespace}{"\t"}{.metadata.name}{"\t"}{.status.containerStatuses[*].state.waiting.reason}{"\n"}{end}' | grep ImagePullBackOff | while IFS=$'\t' read -r namespace pod reason; do
+    # 获取镜像名称列表
+    images=$(kubectl get pod $pod -n $namespace -o=jsonpath='{range .spec.containers[*]}{.image}{"\n"}{end}')
+    
+    # 遍历处理每个镜像
+    while IFS= read -r image; do
+        echo "Image <$image> ImagePullBackOff, pull image with proxy docker.wuxs.workers.dev"
+        image_name=$(echo $image | cut -d':' -f1)
+        image_tag=$(echo $image | cut -d':' -f2)
+        echo "$runtime pull docker.wuxs.workers.dev/$image_name:$image_tag"
+        bash -c "$runtime pull docker.wuxs.workers.dev/$image_name:$image_tag"
+        echo "$runtime tag docker.wuxs.workers.dev/$image_name:$image_tag $image"
+        bash -c "$runtime tag docker.wuxs.workers.dev/$image_name:$image_tag $image"
+    done <<< "$images"
 done
