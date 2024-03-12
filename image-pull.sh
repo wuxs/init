21e138aeb66a15e2e4427a8019f8bba21a54c59f
wuxs <wuxs@wuxs.vip>
2024-03-12 15:11:44 +0800

    Update image-pull.sh

--- image-pull.sh
+++ image-pull.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
 # 该脚本使用ChatGPT生成
+runtime="docker"
+if command -v docker &> /dev/null
+then
+  runtime="docker"
+else
+  runtime="crictl"
+  echo "当前环境未安装 Docker 将使用 crictl 拉取镜像"
+fi
 
 # 检查参数是否为空
 if [ $# -eq 0 ]; then
@@ -14,5 +22,5 @@ image_name=$(echo $image | cut -d':' -f1)
 image_tag=$(echo $image | cut -d':' -f2)
 
 # 拉取镜像并重新命名
-docker pull docker.wuxs.workers.dev/$image_name:$image_tag
-docker tag docker.wuxs.workers.dev/$image_name:$image_tag $image
+bash -c "$runtime pull docker.wuxs.workers.dev/$image_name:$image_tag"
+bash -c "$runtime tag docker.wuxs.workers.dev/$image_name:$image_tag $image"
